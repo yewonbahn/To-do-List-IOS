6561c23b6ab905e8adcdbbe1475e751bc308ed34
yewonbahn <banyaewon98@gmail.com>
2021-09-01 13:03:01 +0900

    tododetail

---
+++ Todolist/TodoDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoDetailViewController.swift
+//  Todolist
+//
+//  Created by 반예원 on 2021/09/01.
+//
+
+import UIKit
+
+class TodoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
