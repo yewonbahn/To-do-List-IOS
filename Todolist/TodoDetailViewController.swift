cd2b127a82e682410cc269207f8ca5e9342b2685
yewonbahn <banyaewon98@gmail.com>
2021-09-02 21:39:14 +0900

    nsentity

--- Todolist/TodoDetailViewController.swift
+++ Todolist/TodoDetailViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoDetailViewController: UIViewController {
-
+    @IBOutlet weak var highButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var normalButton: UIButton!
+    @IBOutlet weak var lowButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func setPriority(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            break
+        case 2:
+            break
+        case 3:
+            break
+            
+        default:
+            break
+            
+        }
     }
-    */
+    
 
+    @IBAction func saveTodo(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Todolist", in: context) else{return}
+        
+       guard let object = NSManagedObject(entity: entityDescription, insertInto: context) as? TodoList else{return}
+        
+        
+        
+        
+    }
+    
+    @IBAction func deleteTodo(_ sender: Any) {
+    }
 }
